805fd4091122a65b36eb78bc8679b8e566e8f89c
Dapples <lamzac26@gmail.com>
2025-12-04 00:27:45 +1100

    removed display classes

--- src/Chip8.h
+++ src/Chip8.h
@@ -1,39 +1,38 @@
 #pragma once
 #include <cstdint>
 #include "constants.h"
-#include "Display.h"
 #include <SDL2/SDL.h>
 
 class Chip8 {
     public:
         void initialise();
         void load(const char *filename);
-        void emulateCycle(Display &display);
+        void emulateCycle();
         void updateGraphics();
         void storeKey();
 
-        uint32_t gfx[WIDTH* HEIGHT]; // 2048
+        uint32_t gfx[WIDTH* HEIGHT] = {}; // 2048
         uint8_t keymap[MAX_SIZE] = {
             SDLK_0, SDLK_1, SDLK_2, SDLK_3, SDLK_4,
             SDLK_5, SDLK_6, SDLK_7, SDLK_8, SDLK_9,
             SDLK_a, SDLK_b, SDLK_c, SDLK_d, SDLK_e,
             SDLK_f
         };
-        uint8_t key[MAX_SIZE];
+        uint8_t key[MAX_SIZE] = {};
 
     private:
-        void executeOperation(uint8_t vX, uint8_t vY, uint8_t n, uint8_t nn, uint8_t nnn, Display &display);
+        void executeOperation(uint8_t vX, uint8_t vY, uint8_t n, uint8_t nn, uint8_t nnn);
         void clearDisplay();
         uint8_t randGen();
         uint16_t opcode;
-        unsigned char memory[MEMORY_SIZE];
-        uint8_t registerV[MAX_SIZE];
-        uint16_t I;
-        uint16_t pc;
-        uint8_t delay_timer;
-        uint8_t sound_timer;
-        uint16_t stack[MAX_SIZE];
-        uint8_t sp;
+        unsigned char memory[MEMORY_SIZE] = {};
+        uint8_t registerV[MAX_SIZE] = {};
+        uint16_t I = {};
+        uint16_t pc = {};
+        uint8_t delay_timer = {};
+        uint8_t sound_timer = {};
+        uint16_t stack[MAX_SIZE] = {};
+        uint8_t sp = {};
         uint8_t font[FONT_SIZE] = {
             0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
             0x20, 0x60, 0x20, 0x20, 0x70, // 1
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
 
-#include "Display.h"
-#include "Window.h"
 #include "Chip8.h"
 
 #include <chrono>
@@ -20,45 +18,16 @@ int main(int argc, char **argv) {
     chip8.initialise();
     chip8.load(argv[1]);
 
-    Window window;
-    Display display;
-    // display.render(window.getWindow());
-    // display.createTexture();
+
 
     // Command Loop
-    uint32_t tick;
-    // if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
-    //     std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-    //     return 1;
-    // }
-    // while(true) {
-        // tick = SDL_GetTicks();
-        // SDL_Event event;
-        // // Check if there has been an event
-        // if (SDL_PollEvent(&event)) {
-        //     if (event.type == SDL_QUIT) {
-        //         break;
-        //     }
-
-        //     if (event.type == SDL_KEYDOWN) {
-        //         for (int i = 0; i < MAX_SIZE; ++i) {
-        //             if (event.key.keysym.sym == chip8.keymap[i]) {
-        //                 chip8.key[i] = 1;
-        //             }
-        //         }
-        //     }
-
-        //     if (event.type == SDL_KEYUP)  {
-        //         for (int i = 0; i < MAX_SIZE; ++i) {
-        //             if (event.key.keysym.sym == chip8.keymap[i]) {
-        //                 chip8.key[i] = 0;
-        //             }
-        //         }
-        //     }
-        // }
-
-        chip8.emulateCycle(display);
+    bool exit = false;
+    while (!exit) {
         // this_thread::sleep_for(std::chrono::microseconds(1200));
-    // }
+        chip8.emulateCycle();
+
+
+    }
+
 
 }
